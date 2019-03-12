fa863e32cab3221e5f4b9490913601d4b179c081
Takeshi SAITO <t.saito@quo-digital.jp>
2019-03-12 09:59:51 +0900

    a

--- app/src/main/java/jp/tsaitoquo/gorugoapp/MainActivity.kt
+++ app/src/main/java/jp/tsaitoquo/gorugoapp/MainActivity.kt
@@ -30,5 +30,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
